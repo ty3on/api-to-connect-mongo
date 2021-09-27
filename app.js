19b36c405953a03927614c698ffa22162edf0ba3
ty3on <91453580+ty3on@users.noreply.github.com>
2021-09-27 17:21:43 +0800

    test2

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-
+const port = process.env.PORT || 3000;
 mongoose.connect(process.env.DATABASE_URL);
 const db = mongoose.connection;
 db.on('error', (error) => console.error(error));
 db.once('open', (error) => console.log('connected to database'));
-app.listen(3000, () => console.log('Server Started'));
+app.listen(port, () => console.log('Server Started'));
 
 app.use(express.json());
 
